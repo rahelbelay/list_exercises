bf1669202f89d3367757f7bafcec2b0a9282baf6
Rahel Belay <rkonjo@gmail.com>
2019-11-11 14:40:14 -0500

    counter and day 4 demo

---
+++ day-3-demo.py
@@ -0,0 +1,75 @@
+# print('1.' )
+# number = 0
+# while number < 10:
+#     number = number + 1
+#     print(number,'\n')
+
+# print('2.' )
+# number = 1
+# while number < 8:
+#     number = number + 1
+#     print(number ,'\n')
+
+# print('3.' )
+
+# number = 0
+# while number < 10:
+#   number = number + 1
+#   if number % 2 != 0:
+#      print(number,'\n')
+# another = 'yes'
+# count = -1
+# while another == 'yes':
+#     count = count + 1
+#     print ('You have %s coins.' % (count))
+#     another = input('Do you need another?')
+# print('Bye')
+
+# size = 5
+# row = 0
+# square = ''
+# while row < size:
+#     column = 0
+#     while column < size:
+#         print('*',end = '')
+#         square += f'{column}'
+#         square += '*'
+#         column += 1
+#     print()
+#     square += '\n'
+#     row += 1
+# print(square)
+
+# print('*',  end='')
+
+
+
+
+
+   
+
+# size = 10
+# row = 0
+# square = ''
+
+# while row < size:
+#     column = 0
+#     while column < size:
+#         print('*', end = '')
+#         column += 1
+#         square += f'{column}'
+#         square += '*'
+#     print()
+#     square += '\n'
+#     row += 1
+# print(square)
+
+def make_name(person,subject):
+    return 'your name is %s and your faverite subject is %s' % (person,subject)
+    print(make_name) 
+
+
+
+
+
+
