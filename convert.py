4dcf1ef38727f0d82117d5ef2f3b040b720e2169
Rahel Belay <rkonjo@gmail.com>
2019-11-11 14:30:43 -0500

    convert strings excersice

---
+++ convert.py
@@ -0,0 +1,11 @@
+
+favorite_number = input("My favorite number?")
+# print(favorite_number)
+favorite_number = int(favorite_number)
+my_favorite_number = favorite_number + 1
+print(my_favorite_number)
+# all inputs treats like string
+# convert string to intger
+# eg int(my_favorite-number)
+print(f"My favorite number is{my_favorite_number}")
+
